5d11a55e182f397d6ec972d122b566c6a4806294
João Pedro Mayrink <pepemayrink@gmail.com>
2021-09-21 02:08:30 -0300

    [#1] feat: criando register-card component v3

--- Codigo/cmf-front/src/app/core/components/components.module.ts
+++ Codigo/cmf-front/src/app/core/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterCardComponent } from './register-card/register-card.component';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterCardComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
   exports: [RegisterCardComponent],
 })
 export class ComponentsModule {}
--- Codigo/cmf-front/src/app/core/components/register-card/register-card.component.ts
+++ Codigo/cmf-front/src/app/core/components/register-card/register-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 import { DEFAULT_USER, User } from 'src/app/types/User';
 
 @Component({
@@ -18,23 +19,34 @@ export class RegisterCardComponent implements OnInit {
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
+      ]),
+      confirmPassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        // this.checkConfirmPassword,
       ])
     }
   );
 
   newUser: User = DEFAULT_USER;
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
-    console.log('to aquiiiii');
+    console.log('to aquiiiii register-card');
   }
 
   onSubmit() {
     console.log('submit pressed');
-    console.log('🚀 -> RegisterCardComponent -> email', this.newUser.email);
-    console.log('🚀 -> RegisterCardComponent -> password', this.newUser.password);
-    window.alert(`sending new user to register endpoint\nemail=${this.newUser.email}\npassword=${this.newUser.password}`);
+    const email: string = this.registerForm.get('email').value;
+    const password: string = this.registerForm.get('password').value;
+    console.log('🚀 -> RegisterCardComponent -> email', email);
+    console.log('🚀 -> RegisterCardComponent -> password', password);
+    window.alert(`sending new user to register endpoint\nemail=${email}\npassword=${password}`);
+
+    this.userService.registerUser(this.newUser);
   }
 
   checkEmail(control: FormControl) {
@@ -45,6 +57,17 @@ export class RegisterCardComponent implements OnInit {
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!re.test(String(value))) {
         return { email: 'true' };
+      }
+    }
+    return null;
+  }
+
+  checkConfirmPassword(confirmPasswordControl: FormControl) {
+    if (!!confirmPasswordControl && !!this.registerForm) {
+      const password: string = this.registerForm.get('password') ? this.registerForm.get('password').value : '';
+      const confirmPassword: string = confirmPasswordControl.value;
+      if (password === confirmPassword) {
+        return { confirmPassword: 'true'};
       }
     }
     return null;
