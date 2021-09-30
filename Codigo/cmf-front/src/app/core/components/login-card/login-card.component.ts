fd68f678a832dae724724caceb26cd3b884d0078
João Pedro Mayrink <pepemayrink@gmail.com>
2021-09-29 22:21:43 -0300

    [#2] feat: construindo interface de login

--- Codigo/cmf-front/src/app/core/components/login-card/login-card.component.ts
+++ Codigo/cmf-front/src/app/core/components/login-card/login-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TopbarService } from 'src/app/services/topbar.service';
+import { checkEmail } from '../../shared/custom-validators/custom-validators-sign-up-form';
 
 @Component({
   selector: 'app-login-card',
@@ -9,17 +11,41 @@ import { TopbarService } from 'src/app/services/topbar.service';
 })
 export class LoginCardComponent implements OnInit {
 
+  loginForm: FormGroup = new FormGroup(
+    {
+      email: new FormControl(null, [
+        Validators.required,
+        checkEmail,
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+      ]),
+    },
+  );
+
+  isMobile: boolean;
+
   constructor(
     private router: Router,
     private topbarService: TopbarService,
   ) { }
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
   ngOnInit() {
+    this.isMobile = window.innerWidth < 767;
     this.topbarService.configBackButton(false);
   }
 
   navigateToRegister() {
     this.router.navigate(['/external/register']);
+  }
+
+  onSubmit() {
+    console.log('submit login');
   }
 
 }
