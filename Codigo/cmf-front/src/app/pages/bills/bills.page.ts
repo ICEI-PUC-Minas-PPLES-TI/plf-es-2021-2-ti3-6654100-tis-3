fa08ecdc91bb794ce1e17f60be9464d5d9d3b3ee
João Pedro Mayrink <pepemayrink@gmail.com>
2021-10-01 00:57:40 -0300

    [#3] criando modal com formulário e regras de validação

--- Codigo/cmf-front/src/app/pages/bills/bills.page.ts
+++ Codigo/cmf-front/src/app/pages/bills/bills.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,11 +9,28 @@ import { Router } from '@angular/router';
 })
 export class BillsPage implements OnInit {
 
+  addTransaction: FormGroup = new FormGroup(
+    {
+      transactionType: new FormControl(null, [
+        Validators.required,
+      ]),
+      transactionValue: new FormControl(null, [
+        Validators.required,
+      ]),
+      relatedProduct: new FormControl(),
+      productName: new FormControl(),
+      productQuantity: new FormControl()
+    },
+  );
+
+  transactionModal: HTMLElement;
+
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit() {
+    this.transactionModal = document.getElementById('add-transaction');
     this.checkUserLogged();
   }
 
@@ -20,6 +38,27 @@ export class BillsPage implements OnInit {
     if (window.localStorage.getItem('userLogged') !== 'true') {
       this.router.navigate(['external/login']);
     }
+  }
+
+  toggleTransactionModal() {
+    this.transactionModal.classList.toggle('hidden');
+  }
+
+  toggleAccordion(id: string) {
+    console.log('abre aí', id);
+    const element = document.getElementById(id);
+    const content = Array.from(element.children).filter(e => e.classList.contains('content'));
+    content[0].classList.toggle('hidden');
+  }
+
+  onAddTransaction() {
+    console.log('ainda preciso fazer isso kkkkkkrying');
+    console.log('🚀 -> BillsPage -> addTransaction', this.addTransaction);
+  }
+
+  preventDefault($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
   }
 }
 
